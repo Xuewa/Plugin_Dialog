ee90dcab7301cfe279edc5971f35df2a957f05fa
Xuewa <366653992@qq.com>
2016-09-11 22:59:42 +0800

    test commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/**
+ *requireJS的入口
+ * by Xuewa 2016-09-11
+ */
+require.config({
+    // baseUrl: 'js/lib',
+    paths: {
+        jquery: 'jquery-1.9.1.min'
+    }
+});
+require(["jquery","Dialog"], function($,d) {
+   // testing引入jq
+   // console.log("jquery!");
+   
+	var d=new d.Dialog({
+		type:"ques",
+		text:"你确定下单吗？",
+		buttons:[
+			{
+				type:"left",
+				text:"取消",
+				callBack:function(){
+
+				}
+			},{
+				type:"right",
+				text:"确认下单",
+				callBack:function(){
+					
+				}
+			}
+		]
+	}).renderUI().on('alert',function(){
+		alert('on function');
+	});
+
+
+});
