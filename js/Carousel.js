a328de0aca5bf82f794c79c5e2d7964f2d65511a
Xuewa <366653992@qq.com>
2016-09-14 22:14:46 +0800

    carousel btns ing

--- js/Carousel.js
+++ js/Carousel.js
@@ -11,13 +11,22 @@ define(["Widget","jquery"],function(w,$){
 	Carousel.prototype=$.extend({},new w.Widget(),{
 		renderUI:function(){
 			this.container=$("<div class='carousel_container'></div>");
-			this.prev_btn=$("<div class='carousel_btn prev_btn'></div>");
-			this.next_btn=$("<div class='carousel_btn next_btn'></div>");
+			this.prev_btn=$("<div class='carousel_btn prev_btn'><span class='prev_icon'></span></div>");
+			this.next_btn=$("<div class='carousel_btn next_btn'><span class='next_icon'></span></div>");
 			this.pics=$("<div class='carousel_pics'></div>");
 			this.pic_list=$("<ul class='carousel_list'></ul>");
 			if(this.config.pics.length>0){
-				this.pic_item=$("<li class='carousel_item'>");
+				for (var i =0;i<= this.config.pics.length - 1; i++) {
+					var pic=this.config.pics[i];
+					var pic_item=$("<li class='carousel_item'><a href='#'><img src='"+pic+"'/></a></li>");
+					this.pic_list.append(pic_item);
+				}
 			}
+			this.container.append(this.prev_btn);
+			this.container.append(this.next_btn);
+			this.pics.append(this.pic_list);
+			this.container.append(this.pics);
+			$("body").append(this.container);
 		},
 		//为Carousel中的元素绑定事件
 		bindUI:function(){
